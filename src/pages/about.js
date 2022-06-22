45c27c5964a04e571974c0b4000773c0029eedd9
yenical <70831667+yenical@users.noreply.github.com>
2022-06-22 17:13:54 +0800

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -27,7 +27,7 @@ const AboutPage = (props) => {
         <Hero
           maxWidth={'900px'}
           image={'/about.png'}
-          title={`Sydney \n A British brand since 1860`}
+          title={`KM and Co \n A British brand since 1860`}
         />
 
         <div className={styles.navContainer}>
