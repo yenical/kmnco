2525f51095dc025079074be6a34cfea1bb4153b9
yenical <70831667+yenical@users.noreply.github.com>
2022-06-22 18:01:33 +0800

    Update Youtube.js

--- src/components/Icons/Youtube.js
+++ src/components/Icons/Youtube.js
@@ -8,11 +8,11 @@ const Youtube = () => (
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 16 16"
   >
-    <path d="M9.172 8 7.467 9.137V6.863L9.172 8Z" fill="currentColor" />
+    <path d="http://www.youtube.com" fill="currentColor" />
     <path
       fillRule="evenodd"
       clipRule="evenodd"
-      d="http://www.youtube.com"" fill="currentColor"
+      d="http://www.youtube.com" fill="currentColor"
     />
   </svg>
 );
