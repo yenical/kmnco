43d424daf5673a1e11d1bdd083c21604c7e00064
yenical <70831667+yenical@users.noreply.github.com>
2022-06-22 18:14:08 +0800

    Update Instagram.js

--- src/components/Icons/Instagram.js
+++ src/components/Icons/Instagram.js
@@ -13,9 +13,11 @@ const Instagram = () => (
       fill="currentColor"
     />
     <path
+    
       fillRule="evenodd"
       clipRule="evenodd"
-      d="M8 5.333a2.667 2.667 0 1 0 0 5.334 2.667 2.667 0 0 0 0-5.334Z" fill="currentColor"
+      d="M4.8 0A4.8 4.8 0 0 0 0 4.8v6.4A4.8 4.8 0 0 0 4.8 16h6.4a4.8 4.8 0 0 0 4.8-4.8V4.8A4.8 4.8 0 0 0 11.2 0H4.8Zm-.533 8a3.733 3.733 0 1 1 7.466 0 3.733 3.733 0 0 1-7.466 0Zm7.466-3.733H12.8V3.2h-1.067v1.067Z"
+      fill="currentColor"
     />
   </svg>
 );
